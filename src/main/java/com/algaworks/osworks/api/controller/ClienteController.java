9fab72b661db25c4a81bad0a64e9fbc6a8321b4c
JoÃ£o Vinaud <leyenasd@gmail.com>
2020-04-22 08:58:01 -0300

    Resultados não encontrados definidos

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,9 +30,12 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/clientes/{clienteId}")
-	public Cliente buscar(@PathVariable Long clienteId) {
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 
-		return cliente.orElse(null);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
