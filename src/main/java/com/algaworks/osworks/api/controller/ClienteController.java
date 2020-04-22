45cbfa697030b61d5943af2a7508efc0416082ed
JoÃ£o Vinaud <leyenasd@gmail.com>
2020-04-22 08:58:01 -0300

    Endpoint de busca por id

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -2,12 +2,14 @@ package com.algaworks.osworks.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -23,6 +25,13 @@ public class ClienteController {
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return clienteRepository.findByNomeContaining("Law");
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+		return cliente.orElse(null);
 	}
 }
