577caf71e6f8cb4e7e64f17819d7ccd085c0bc2c
JoÃ£o Vinaud <leyenasd@gmail.com>
2020-04-22 08:58:01 -0300

    Consulta de clinete no banco

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +14,11 @@ import com.algaworks.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager  manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao snow");
-		cliente1.setEmail("email@email.com");
-		cliente1.setTelefone("+5511912345678");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("email2@email.com");
-		cliente2.setTelefone("+551191235433");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
